ee300de3580b2e47ae8538a54fe597818cbdd54c
Brian Merchant <bhmerchant@gmail.com>
2022-11-01 11:54:01 -0700

    MNWE

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![forbid(missing_docs)]
+//! A test to see if `#[automatically_derived]` is sufficient to silent `forbid(missing_docs)`.
+use bevy::prelude::*;
+use bevy::ecs::query::WorldQuery;
+
+/// A dummy component to query for.
+#[derive(Clone, Copy, Default, Debug, Component)]
+pub struct DummyComponent;
+
+/// A dummy query on which we will derive [`WorldQuery`].
+#[derive(WorldQuery)]
+pub struct DummyQuery {
+    /// Gotta query 'em all!
+    dummy: DummyComponent,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
